33c2fd5f39a1775272550c155eb02ae7559e70c2
Marcin Świrek <swirek.marcin@gmail.com>
2018-06-20 22:59:03 +0200

    Add CountryDetaild component

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import routes from './routes';
 
 render(
   <Provider store={store}>
-    <Router history={hashHistory} routes={routes} />
-        
+    <Router history={hashHistory} routes={routes} /> 
   </Provider>,
   document.getElementById("root")
 );
