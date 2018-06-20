f1c88d97b6d586000d2565e06027998b11a86d96
Marcin Świrek <swirek.marcin@gmail.com>
2018-06-20 23:14:40 +0200

    Add Route to new CountryDetailContainer

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,15 @@ import Home from "./presentational/home.component";
 import Contact from "./presentational/contact.component";
 import NotFound from "./presentational/not-found.component";
 import CountryFlagContainer from "./containers/flag-container.component";
+import CountryDetailContainer from './containers/country-detail-container.component';
 
 export default (
   <Route path="/" component={Navigation}>
     <IndexRoute component={Home} />
-    <Route path="countries" component={CountryFlagContainer} />
+    <Route path="countries" >
+        <IndexRoute component={CountryFlagContainer} />
+        <Route path='country/:id' component={CountryDetailContainer} />
+    </Route>
     <Route path="contact" component={Contact} />
     <Route path="*" component={NotFound} />
   </Route>
