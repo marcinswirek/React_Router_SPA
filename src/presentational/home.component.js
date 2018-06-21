6e8f74ae2cf31046ca58f68892fb9877795424c8
Marcin Świrek <swirek.marcin@gmail.com>
2018-06-21 14:38:06 +0200

    Import style sheet

--- src/presentational/home.component.js
+++ src/presentational/home.component.js
@@ -1,4 +1,5 @@
 import React from "react";
+import style from '../country.css';
 
 const Home = () => <h1>Welcome to Countries App</h1>;
 
--- src/presentational/not-found.component.js
+++ src/presentational/not-found.component.js
@@ -1,4 +1,5 @@
 import React from "react";
+import style from '../country.css';
 
 const NotFound = () => <h1>Error 404...Not found</h1>;
 
